a134cb1951a844e22ab9ba9572afdd70a0fc80c0
tharshan <tharshanrobert001@gmail.com>
2021-05-04 21:46:39 +0530

    1 st push

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="blue-grey darken-4">
+      <div className="center">
+        <p className="white-text">&copy; 2021 GYPZEEZ </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
